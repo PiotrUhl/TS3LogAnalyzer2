ae60390db2ad8e3b4c7229d4a9a88bb911337735
Piotr Uhl <piotruhl@gmail.com>
2020-05-19 17:57:54 +0200

    Log jest singletonem

--- TS3LogAnalyser2/Log.h
+++ TS3LogAnalyser2/Log.h
@@ -1,4 +1,21 @@
 #pragma once
+//singletonowy log
 class Log {
+#pragma region singleton
+private:
+	Log();
+public:
+	Log(const Log&) = delete;
+	Log& operator=(const Log&) = delete;
+	Log(Log&&) = delete;
+	Log& operator=(Log&&) = delete;
+
+	static Log& getInstance() {
+		static Log instance;
+		return instance;
+	}
+private:
+#pragma endregion
+
 };
 
