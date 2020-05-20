fa8adf5925fe142d645b1f87f35e96d2490a01ea
Piotr Uhl <piotruhl@gmail.com>
2020-05-20 14:48:55 +0200

    Revert "Dodano wyjątek niezidentyfikowanej linii"
    This reverts commit 45f92969f91fd8a4dd1db35864fe0aa7c23f606a.

--- TS3LogAnalyser2/LineInterpreter.cpp
+++ TS3LogAnalyser2/LineInterpreter.cpp
@@ -12,8 +12,3 @@ LineInfo LineInterpreter::interpretLine(Line line) const {
 RecordType LineInterpreter::checkRecordType(Line line) const {
 	return RecordType();
 }
-
-//returns unidentified line
-std::string LineInterpreter::UnidentifiedLineException::line() {
-	return _line;
-}
